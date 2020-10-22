8e9245989580ae5c319d8e6565fed3fede677d11
a.gheynarloo <a.gheynarloo@hamisystem.ir>
2020-10-22 19:08:18 +0330

    add calulator

--- app.js
+++ app.js
@@ -1,5 +1,35 @@
-var x=box1.number;
-var y=box2.number;
-function myfunk() {
-   box3.number=x*y; 
+function calulator(symbol) {
+  var number1 = document.getElementById("box1").value;
+  var number2 = document.getElementById("box2").value;
+
+  var result = 0;
+  if (symbol == "*") {
+    result = number1 * number2;
+  } else if (symbol == "+") {
+    result = Number(number1) + Number(number2);
+  } else if (symbol == "/") {
+    result = Number(number1) / Number(number2);
+  } else if (symbol == "-") {
+    result = Number(number1) - Number(number2);
+  }
+
+  document.getElementById("box3").value = result;
+
+//   return 1;
 }
+
+// function myfunk() {
+//   document.getElementById("box3").value = calulator("*");
+// }
+
+// function myfunk2() {
+//   document.getElementById("box3").value = calulator("+");
+// }
+
+// function myfunk3() {
+//   document.getElementById("box3").value = calulator("/");
+// }
+
+// function myfunk4() {
+//   document.getElementById("box3").value = calulator("-");
+// }
