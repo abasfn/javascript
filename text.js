650d9c84f4db71eb1f070441ce3fff20ef57d193
abasfn <fakharanabas@gmail.com>
2020-10-27 15:08:01 +0330

    add kamek project

--- text.js
+++ text.js
@@ -1,10 +1,38 @@
 function myfunk() {
     var x=document.getElementById("box1").value;
-    document.getElementById("boxs2").value=x;
+
 
 
     var node=document.createElement("p");
     var textnode = document.createTextNode(x);
     node.appendChild(textnode);
     document.getElementById("boxs2").appendChild(node);
+ 
 }
+function myfunk2(amalgar) {
+    var list = document.getElementById("boxs2");
+    if (amalgar=="1") {
+       list.removeChild(list.childNodes[0]);  
+    }
+   else if (amalgar=="2") {
+    list.removeChild(list.childNodes[1]);   
+   }
+   else if (amalgar=="3") {
+    list.removeChild(list.childNodes[2]);   
+   }
+   else if (amalgar=="4") {
+    list.removeChild(list.childNodes[3]);   
+   }
+   else if (amalgar=="5") {
+    list.removeChild(list.childNodes[4]);   
+   }
+   else if (amalgar=="6") {
+    list.removeChild(list.childNodes[5]);   
+   }
+   else if (amalgar=="7") {
+    list.removeChild(list.childNodes[6]);   
+   }
+}
+
+
+
