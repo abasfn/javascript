bc514c1e93e10490a4669e8a49c8621edc1a5293
abasfn <fakharanabas@gmail.com>
2020-10-27 17:40:46 +0330

    add color javascript

---
+++ indexs.js
@@ -0,0 +1,11 @@
+function myfunk(amalgar) {
+    if (amalgar=="red") {
+       document.getElementById("all").style.background="red"; 
+    }
+    else if (amalgar=="blue") {
+        document.getElementById("all").style.background="blue"; 
+    }
+    else if (amalgar=="black") {
+        document.getElementById("all").style.background="black"; 
+    } 
+}
