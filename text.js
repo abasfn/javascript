751063d665a2fa267c15412daa585c25f34a2be8
a.gheynarloo <amin.gheynarloo@gmail.com>
2020-10-28 03:53:29 +0330

    add remove button refactor code

--- text.js
+++ text.js
@@ -1,38 +1,42 @@
 function myfunk() {
-    var x=document.getElementById("box1").value;
+  var x = document.getElementById("box1").value;
 
+  var node = document.createElement("div");
+  var button = document.createElement("button");
+  button.textContent = "remove";
+  button.classList.add("remove");
 
+  button.addEventListener("click", function() {
+    node.remove();
+  })
 
-    var node=document.createElement("p");
-    var textnode = document.createTextNode(x);
-    node.appendChild(textnode);
-    document.getElementById("boxs2").appendChild(node);
- 
+  var textnode = document.createTextNode(x);
+  node.appendChild(textnode);
+  node.appendChild(button);
+  document.getElementById("boxs2").appendChild(node);
 }
+
+
+
 function myfunk2(amalgar) {
-    var list = document.getElementById("boxs2");
-    if (amalgar=="1") {
-       list.removeChild(list.childNodes[0]);  
-    }
-   else if (amalgar=="2") {
-    list.removeChild(list.childNodes[1]);   
-   }
-   else if (amalgar=="3") {
-    list.removeChild(list.childNodes[2]);   
-   }
-   else if (amalgar=="4") {
-    list.removeChild(list.childNodes[3]);   
-   }
-   else if (amalgar=="5") {
-    list.removeChild(list.childNodes[4]);   
-   }
-   else if (amalgar=="6") {
-    list.removeChild(list.childNodes[5]);   
-   }
-   else if (amalgar=="7") {
-    list.removeChild(list.childNodes[6]);   
-   }
+  var list = document.getElementById("boxs2");
+  //if(amalgar <= 6 && amalgar >= 0) {
+      list.removeChild(list.childNodes[amalgar-1]);
+  //}
+
+//   if (amalgar == "1") {
+//     list.removeChild(list.childNodes[0]);
+//   } else if (amalgar == "2") {
+//     list.removeChild(list.childNodes[1]);
+//   } else if (amalgar == "3") {
+//     list.removeChild(list.childNodes[2]);
+//   } else if (amalgar == "4") {
+//     list.removeChild(list.childNodes[3]);
+//   } else if (amalgar == "5") {
+//     list.removeChild(list.childNodes[4]);
+//   } else if (amalgar == "6") {
+//     list.removeChild(list.childNodes[5]);
+//   } else if (amalgar == "7") {
+//     list.removeChild(list.childNodes[6]);
+//   }
 }
-
-
-
