d586a7394202ad56d5fce184c0025832a7041d1c
a.gheynarloo <amin.gheynarloo@gmail.com>
2020-10-28 16:22:12 +0330

    change childNodes with children

--- 3/index.js
+++ 3/index.js
@@ -1,6 +1,6 @@
 function myfunk2(amalgar) {
     var list = document.getElementById("row1");
-    list.removeChild(list.childNodes[amalgar-1]);
+    list.removeChild(list.children[amalgar-1]);
  
  
 }
