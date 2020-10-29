eeb6eff1a244f43b50b1398dd35f10a92dee6b45
abasfn <fakharanabas@gmail.com>
2020-10-29 15:50:30 +0330

    add right of project

--- index.js
+++ index.js
@@ -1,11 +1,52 @@
+
+
 function remove(index) {
-    var item = document.getElementById("row1" + index);
+    var id = ("boxs" + index)
+    var item = document.getElementById(id);
+
     item.remove();
-    }
+}
+
+
+function add() {
+
+    var rot = document.getElementById("row1");
+    var node = document.createElement("li");
+    node.classList.add("boxs1");
+
+    var div = document.createElement("div");
+    div.classList.add("content");
+
+    var nav = document.createElement("nav");
+
+
+    var p = document.createElement("p");
+
+    p.innerText = "abas ";
+
+    var button = document.createElement("button");
+    p.classList.add("button");
+    button.innerText = "remove";
+    button.addEventListener("click", function () {
+
+        node.remove();
+    });
+    div.appendChild(nav)
+    div.appendChild(p);
+    div.appendChild(button);
+    node.appendChild(div);
+    rot.appendChild(node);
+}
 
 
 
-    
-    
-    
-    
+
+
+
+
+
+
+
+
+
+
