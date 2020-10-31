52a0bf6f7c1a946bd8df1925a8162b3d8aeaef87
abasfn <fakharanabas@gmail.com>
2020-10-31 20:34:35 +0330

    add java

---
+++ index.js
@@ -0,0 +1,15 @@
+
+var g=document.getElementsByClassName("paragraf2");
+
+g[0].style.color = "red"; 
+
+var p=document.getElementsByTagName("h1");
+p[0].innerText="helliw";
+
+
+var h2=document.querySelectorAll("h2");
+h2[1].style.color="red";
+
+
+  
+
