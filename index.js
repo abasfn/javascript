7c554c02b2959030f62125b845e15c51bf1102a7
abasfn <fakharanabas@gmail.com>
2020-11-07 12:29:21 +0330

    right of project

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function mfunk() {
 var x=document.forms["myForm"]["email"].value;
 var atpos=x.indexOf("@");
 var dotpos=x.lastIndexOf(".");
-if (atpos<1 || dotpos<atpos+2 || dotpos+2>=x.length)
+if (atpos<=1 || dotpos<atpos+2 || dotpos+2>=x.length)
   {
     toastr.warning('this is not Gmail')
   return false;
