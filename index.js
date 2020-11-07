03294a067e4133e995703309a06764f118168c20
abasfn <fakharanabas@gmail.com>
2020-11-07 11:25:08 +0330

    add feature/register

---
+++ index.js
@@ -0,0 +1,6 @@
+function mfunk() {
+    var x =document.getElementsByTagName("input");
+    console.log(x[0].value)
+    console.log(x[1].value)
+    console.log(x[2].value)
+}
--- toastrsample.js
+++
@@ -1,9 +0,0 @@
-function myfunk() {
-  
-  toastr.warning('My name is Inigo Montoya. You killed my father, prepare to die!')
-}
-
-function success() {
-  
-  toastr.success('My name is Inigo Montoya. You killed my father, prepare to die!')
-}
