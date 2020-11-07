bc09a32cca5ec0bb20c8c57b7ec5361c9cf40f01
abasfn <fakharanabas@gmail.com>
2020-11-07 17:36:18 +0330

    add right of project 2

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
-function mfunk() { 
-
-
-var x=document.forms["myForm"]["email"].value;
-var atpos=x.indexOf("@");
-var dotpos=x.lastIndexOf(".");
-if (atpos<=1 || dotpos<atpos+2 || dotpos+2>=x.length)
-  {
-    toastr.warning('this is not Gmail')
-  return false;
-  }
-
-
+var testresults
+function checkemail(){
+    var str = document.validation.emailcheck.value
+    var filter = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i
+    if (filter.test(str))
+        testresults = true
+    else {
+        toastr.warning('this is not Gmail')
+        testresults = false
+        return (testresults)
+    }
+    
 
     var x =document.getElementsByTagName("input");
     console.log(x[0].value)
     console.log(x[1].value)
     console.log(x[2].value)
+
+
+
+
+}
+function checkbae(){
+    if (document.layers || document.getElementById || document.all)
+        return checkemail()
+    else
+        return true
 }
 
 
