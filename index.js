5448b2a208db452ecf60c374a7136d54ab9b0cfa
abasfn <fakharanabas@gmail.com>
2020-11-07 12:20:53 +0330

    add java

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-function mfunk() {
+function mfunk() { 
+
+
+var x=document.forms["myForm"]["email"].value;
+var atpos=x.indexOf("@");
+var dotpos=x.lastIndexOf(".");
+if (atpos<1 || dotpos<atpos+2 || dotpos+2>=x.length)
+  {
+    toastr.warning('this is not Gmail')
+  return false;
+  }
+
+
+
     var x =document.getElementsByTagName("input");
     console.log(x[0].value)
     console.log(x[1].value)
     console.log(x[2].value)
 }
+
+
