71cd80f545c68619e6339f9b79dac0c275784f33
abasfn <fakharanabas@gmail.com>
2020-11-19 09:42:34 +0330

    add project

---
+++ index.js
@@ -0,0 +1,15 @@
+
+function myfunk() {
+ var x= document.getElementsByTagName("input");
+ x[0].value=Math.floor(Math.random() * 100);
+ x[1].value=Math.floor(Math.random() * 100);
+ x[2].value=Math.floor(Math.random() * 100);
+ x[3].value=Math.floor(Math.random() * 100);
+ x[4].value=Math.floor(Math.random() * 100);
+ x[5].value=Math.floor(Math.random() * 100);
+ x[6].value=Math.floor(Math.random() * 100);
+ x[7].value=Math.floor(Math.random() * 100);
+ x[8].value=Math.floor(Math.random() * 100);
+ x[9].value=Math.floor(Math.random() * 100);
+ x[10].value=Math.floor(Math.random() * 100);
+}
